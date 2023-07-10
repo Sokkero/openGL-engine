65bbc1a9c3b5625da0149b59041284bd893bacb7
Paul Sohns <paul.sohns@web.de>
2023-07-10 16:14:37 +0200

    Code formatting

--- src/classes/engine/WindowManager.cpp
+++ src/classes/engine/WindowManager.cpp
@@ -1,16 +1,14 @@
-
 #include "WindowManager.h"
 
-#include <utility>
-
-namespace Engine {
+namespace Engine
+{
     WindowManager::WindowManager(std::string windowTitle, int windowWidth, int windowHeight, int textureSamples)
-    : m_gameWindow(nullptr)
-    , m_windowDimensions(std::pair<int, int>(windowWidth, windowHeight))
-    , m_textureSamples(textureSamples)
-    , m_windowTitle(std::move(windowTitle))
+            : m_gameWindow(nullptr)
+            , m_windowDimensions(std::pair<int, int>(windowWidth, windowHeight))
+            , m_textureSamples(textureSamples)
+            , m_windowTitle(std::move(windowTitle))
     {
-    };
+    }
 
     void WindowManager::setWindowDimensions(int width, int height)
     {
@@ -22,7 +20,7 @@ namespace Engine {
     {
         // Initialise GLFW
         glewExperimental = true; //Needed for core profile
-        if ( !glfwInit() )
+        if (!glfwInit())
         {
             fprintf(stderr, "Failed to initialize GLFW!\n");
             return -1;
@@ -30,12 +28,18 @@ namespace Engine {
 
         glfwWindowHint(GLFW_SAMPLES, m_textureSamples); // Anti-Aliasing
         glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); // Use version 3.3
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR,3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
         glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
         glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-        m_gameWindow = glfwCreateWindow(m_windowDimensions.first, m_windowDimensions.second, m_windowTitle.c_str(), nullptr, nullptr);
-        if ( m_gameWindow == nullptr )
+        m_gameWindow = glfwCreateWindow(
+                m_windowDimensions.first,
+                m_windowDimensions.second,
+                m_windowTitle.c_str(),
+                nullptr,
+                nullptr
+        );
+        if (m_gameWindow == nullptr)
         {
             fprintf(stderr, "Failed to open GLFW window...\n");
             glfwTerminate();
@@ -46,11 +50,11 @@ namespace Engine {
         glewExperimental = true; // Needed in the core profile
         if (glewInit() != GLEW_OK)
         {
-            fprintf( stderr, "Failed to initialize GLEW...\n" );
+            fprintf(stderr, "Failed to initialize GLEW...\n");
             return -1;
         }
 
-        glfwSetInputMode( m_gameWindow, GLFW_STICKY_KEYS, GL_TRUE);
+        glfwSetInputMode(m_gameWindow, GLFW_STICKY_KEYS, GL_TRUE);
 
         //engineManager* EngineManager = engineManager::getEngineManager();
 
@@ -61,7 +65,7 @@ namespace Engine {
             glfwSwapBuffers(m_gameWindow);
             glfwPollEvents();
         }
-        while( glfwGetKey(m_gameWindow, GLFW_KEY_ESCAPE ) != GLFW_PRESS && glfwWindowShouldClose(m_gameWindow) == 0 );
+        while (glfwGetKey(m_gameWindow, GLFW_KEY_ESCAPE) != GLFW_PRESS && glfwWindowShouldClose(m_gameWindow) == 0);
 
         return 0;
     }
--- src/classes/engine/WindowManager.h
+++ src/classes/engine/WindowManager.h
@@ -8,28 +8,33 @@
 #include <glm/vec2.hpp>
 #include <GLFW/glfw3.h>
 
-namespace Engine {
+namespace Engine
+{
 
-    class WindowManager {
-    public:
-        explicit WindowManager(
-            std::string windowTitle = "My little Engine",
-            int windowWidth = 1024,
-            int windowHeight = 640,
-            int textureSamples = 4
-        );
-        ~WindowManager() = default;
+    class WindowManager
+    {
+        public:
+            explicit WindowManager(
+                    std::string windowTitle = "My little Engine",
+                    int windowWidth = 1024,
+                    int windowHeight = 640,
+                    int textureSamples = 4
+            );
 
-        int startWindow();
-        void setWindowDimensions(int width, int height);
+            ~WindowManager() = default;
 
-        GLFWwindow* getWindow() { return m_gameWindow; };
-        std::pair<int, int> getWindowDimensions() { return m_windowDimensions; };
+            int startWindow();
 
-    private:
-        GLFWwindow* m_gameWindow;
-        std::pair<int, int> m_windowDimensions;
-        int m_textureSamples;
-        std::string m_windowTitle;
+            void setWindowDimensions(int width, int height);
+
+            GLFWwindow* getWindow() { return m_gameWindow; };
+
+            std::pair<int, int> getWindowDimensions() { return m_windowDimensions; };
+
+        private:
+            GLFWwindow* m_gameWindow;
+            std::pair<int, int> m_windowDimensions;
+            int m_textureSamples;
+            std::string m_windowTitle;
     };
 }
