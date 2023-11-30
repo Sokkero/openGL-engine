76aed1ac747b16a4b3662a1b7d9a9d156fb9b18a
Paul Sohns <paul.sohns@web.de>
2023-11-30 23:22:06 +0100

    Implement the observer pattern for better UserEvent management

--- src/classes/engine/UserEventManager.h
+++ src/classes/engine/UserEventManager.h
@@ -11,6 +11,8 @@ namespace Engine
     class UserEventManager
     {
         public:
+            using Callback = std::function<void()>;
+
             UserEventManager() = default;
             ~UserEventManager() = default;
 
@@ -21,6 +23,15 @@ namespace Engine
 
             void updateEvents(GLFWwindow* window);
 
+            void checkListeners();
+
+            void addListener(std::pair<GLFW_KEY, GLFW_ACTION> action, Callback listener)
+            {
+                m_listeners.emplace_back(std::pair<std::pair<GLFW_KEY, GLFW_ACTION>, Callback>(action, listener));
+            };
+
+            void clearListener() { m_listeners.clear(); };
+
             const std::map<GLFW_KEY, GLFW_ACTION>& getUserEvents() const { return m_userEvents; };
 
             GLFW_ACTION getUserEvent(GLFW_KEY key);
@@ -29,5 +40,6 @@ namespace Engine
 
         private:
             std::map<GLFW_KEY, GLFW_ACTION> m_userEvents;
+            std::vector<std::pair<std::pair<GLFW_KEY, GLFW_ACTION>, Callback>> m_listeners;
     };
 } // namespace Engine
--- src/customCode/testScene/CameraActor.cpp
+++ src/customCode/testScene/CameraActor.cpp
@@ -4,36 +4,37 @@
 #include "../../classes/engine/UserEventManager.h"
 #include "../../classes/engine/WindowManager.h"
 
-void CameraActor::start() {}
-
-void CameraActor::update()
+void CameraActor::start()
 {
-    if(getUserEventManager()->getUserEvent(GLFW_KEY_W) > 0)
-    {
-        getParentNode()->rotateObj(getRight(), getEngineManager()->getDeltaTime() * 100);
-        // moveObj((getForward() * m_engineManager->getDeltaTime()) * 10.f);
-    }
-    if(getUserEventManager()->getUserEvent(GLFW_KEY_S) > 0)
-    {
-        getParentNode()->rotateObj(getLeft(), getEngineManager()->getDeltaTime() * 100);
-        // moveObj((getBackwards() * m_engineManager->getDeltaTime()) * 10.f);
-    }
-    if(getUserEventManager()->getUserEvent(GLFW_KEY_A) > 0)
-    {
-        getParentNode()->rotateObj(getUp(), getEngineManager()->getDeltaTime() * 100);
-        // moveObj((getLeft() * m_engineManager->getDeltaTime()) * 10.f);
-    }
-    if(getUserEventManager()->getUserEvent(GLFW_KEY_D) > 0)
-    {
-        getParentNode()->rotateObj(getDown(), getEngineManager()->getDeltaTime() * 100);
-        // moveObj((getRight() * m_engineManager->getDeltaTime()) * 10.f);
-    }
-    if(getUserEventManager()->getUserEvent(GLFW_KEY_E) > 0)
-    {
-        moveObj((getForward() * getEngineManager()->getDeltaTime()) * 10.f);
-    }
-    if(getUserEventManager()->getUserEvent(GLFW_KEY_Q) > 0)
-    {
-        moveObj((getBackwards() * getEngineManager()->getDeltaTime()) * 10.f);
-    }
+    const auto& wCallback =
+            ([this]() { getParentNode()->rotateObj(getRight(), getEngineManager()->getDeltaTime() * 100); });
+    const auto& sCallback =
+            ([this]() { getParentNode()->rotateObj(getLeft(), getEngineManager()->getDeltaTime() * 100); });
+    const auto& aCallback =
+            ([this]() { getParentNode()->rotateObj(getUp(), getEngineManager()->getDeltaTime() * 100); });
+    const auto& dCallback =
+            ([this]() { getParentNode()->rotateObj(getDown(), getEngineManager()->getDeltaTime() * 100); });
+    const auto& eCallback = ([this]() { moveObj((getForward() * getEngineManager()->getDeltaTime()) * 10.f); });
+    const auto& qCallback = ([this]()
+                             { moveObj((getBackwards() * getEngineManager()->getDeltaTime()) * 10.f); });
+
+    getUserEventManager()->addListener(std::pair<int, int>(GLFW_KEY_W, GLFW_PRESS), wCallback);
+    getUserEventManager()->addListener(std::pair<int, int>(GLFW_KEY_W, GLFW_REPEAT), wCallback);
+
+    getUserEventManager()->addListener(std::pair<int, int>(GLFW_KEY_S, GLFW_PRESS), sCallback);
+    getUserEventManager()->addListener(std::pair<int, int>(GLFW_KEY_S, GLFW_REPEAT), sCallback);
+
+    getUserEventManager()->addListener(std::pair<int, int>(GLFW_KEY_A, GLFW_PRESS), aCallback);
+    getUserEventManager()->addListener(std::pair<int, int>(GLFW_KEY_A, GLFW_REPEAT), aCallback);
+
+    getUserEventManager()->addListener(std::pair<int, int>(GLFW_KEY_D, GLFW_PRESS), dCallback);
+    getUserEventManager()->addListener(std::pair<int, int>(GLFW_KEY_D, GLFW_REPEAT), dCallback);
+
+    getUserEventManager()->addListener(std::pair<int, int>(GLFW_KEY_E, GLFW_PRESS), eCallback);
+    getUserEventManager()->addListener(std::pair<int, int>(GLFW_KEY_E, GLFW_REPEAT), eCallback);
+
+    getUserEventManager()->addListener(std::pair<int, int>(GLFW_KEY_Q, GLFW_PRESS), qCallback);
+    getUserEventManager()->addListener(std::pair<int, int>(GLFW_KEY_Q, GLFW_REPEAT), qCallback);
 }
+
+void CameraActor::update() {}
