c11a7cab944496185340781413e37f9d75ace0f6
Paul Sohns <paul.sohns@web.de>
2024-01-21 00:41:11 +0100

    Add function for tri depth sorting

--- src/classes/helper/ObjectData.h
+++ src/classes/helper/ObjectData.h
@@ -4,8 +4,7 @@
 #include <vector>
 
 #include <GL/glew.h>
-#include <glm/vec2.hpp>
-#include <glm/vec3.hpp>
+#include <glm/glm.hpp>
 
 #include "TriDataDef.h"
 
@@ -49,4 +48,24 @@ namespace Engine
 
             int getVertexCount() const { return int(m_vertexIndices.size() * 3); };
     };
+
+    static bool depthSortTriangles(const glm::vec3& cameraPos, const glm::vec3& nodePos, const std::vector<glm::vec3>& vertices, const triData& triA, const triData& triB)
+    {
+        auto aPos = (vertices[std::get<0>(triA)] + vertices[std::get<1>(triA)] + vertices[std::get<2>(triA)]);
+        aPos.x = aPos.x / 3;
+        aPos.y = aPos.y / 3;
+        aPos.z = aPos.z / 3;
+        aPos += nodePos;
+
+        auto bPos = (vertices[std::get<0>(triB)] + vertices[std::get<1>(triB)] + vertices[std::get<2>(triB)]);
+        bPos.x = bPos.x / 3;
+        bPos.y = bPos.y / 3;
+        bPos.z = bPos.z / 3;
+        bPos += nodePos;
+
+        const auto& distanceA = glm::distance(aPos, cameraPos);
+        const auto& distanceB = glm::distance(bPos, cameraPos);
+
+        return distanceA < distanceB;
+    }
 } // namespace Engine
